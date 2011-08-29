d66d5cdc4be4d49c383b384bf6da77f1d49a9681
Saranyan <saranyan13@gmail.com>
2011-08-29 13:29:11 -0500

    Adding company search

--- lib/linked_in/search.rb
+++ lib/linked_in/search.rb
@@ -17,6 +17,23 @@ module LinkedIn
 
       Mash.from_json(result_json)
     end
+    
+    #adding company search option
+    def csearch(options={})
+      path = "/company-search"
+
+      if options.is_a?(Hash)
+        fields = options.delete(:fields)
+        path += field_selector(fields) if fields
+      end
+      
+      options = { :keywords => options } if options.is_a?(String)
+      options = format_options_for_query(options)
+
+      result_json = get(to_uri(path, options))
+
+      Mash.from_json(result_json)
+    end
 
   end
 
